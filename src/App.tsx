09f9a4a6a5bfa529199777a912749ab8076a2e2e
psm1st <145013061+psm1st@users.noreply.github.com>
2024-11-19 00:51:51 +0900

    init : 개발 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { AuthProvider } from "./AuthContext";
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    background-color: black;
+  }
+
+  #root {
+    width: 360px;
+    height: 640px;
+    background-color: white;
+    border: 1px solid #ccc;
+  }
+`;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <main />,
+  },
+]);
+function App() {
+  return (
+    <div>
+      <GlobalStyles>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </GlobalStyles>
+    </div>
+  );
+}
+
+export default App;
