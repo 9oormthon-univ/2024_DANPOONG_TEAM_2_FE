81f4e12bfe3e16e84b7366c1d01b36cc546bac8b
Hyun Sang <pycandyq@naver.com>
2024-11-19 14:44:46 +0900

    Design: 로그인페이지 퍼블리싱 완료

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,69 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import {
+  LoginContainer,
+  Logo,
+  TabContainer,
+  Tab,
+  Input,
+  CheckboxContainer,
+  Checkbox,
+  LoginButton,
+  LinkContainer,
+  SocialLoginContainer,
+  SocialButton,
+  Tag,
+  CustomCheckbox,
+} from "./LoginStyle";
 
 const Login: React.FC = () => {
-    return (
-        <div style={{color: 'black'}}>
-            로그인 페이지
-        </div>
-    )
-}
+  const [activeTab, setActiveTab] = useState("investor"); // 투자자 회원 기본값
+
+  return (
+    <LoginContainer>
+      <Logo src="/assets/moa_logo.png" alt="MOA Logo" />
+      <TabContainer>
+        <Tab
+          active={activeTab === "investor"}
+          onClick={() => setActiveTab("investor")}
+        >
+          투자자 회원
+        </Tab>
+        <Tab
+          active={activeTab === "company"}
+          onClick={() => setActiveTab("company")}
+        >
+          기업 회원
+        </Tab>
+      </TabContainer>
+      <Input type="text" placeholder="ID(E-Mail)" />
+      <Input type="password" placeholder="Password" />
+      <CheckboxContainer>
+        <Checkbox id="auto-login" />
+        <CustomCheckbox htmlFor="auto-login" />
+        자동 로그인
+      </CheckboxContainer>
+      <LoginButton>로그인</LoginButton>
+      <LinkContainer>
+        <span>아이디 찾기</span>
+        <span>|</span>
+        <span>비밀번호 찾기</span>
+        <span>|</span>
+        <span>회원가입</span>
+      </LinkContainer>
+      <SocialLoginContainer>
+        <SocialButton bgColor="#FFD400">
+          <img src="/assets/kakao_logo.png" alt="Kakao Login" />
+        </SocialButton>
+        <SocialButton bgColor="#000000">
+          <img src="/assets/apple_logo.png" alt="Apple Login" />
+        </SocialButton>
+        <SocialButton bgColor="#f2f2f2">
+          <img src="/assets/google_logo.png" alt="Google Login" />
+        </SocialButton>
+      </SocialLoginContainer>
+      <Tag>소셜 아이디어로 간편 로그인</Tag>
+    </LoginContainer>
+  );
+};
 
 export default Login;
