49d1c8a1b748a589353fec95fbf47683030063ea
yongmin01 <heart77123@naver.com>
2024-11-20 04:51:59 +0900

    Design: 지도 페이지 검색 및 필터 ui 퍼블리싱

---
+++ src/pages/mapPage/MapPage.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Search = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  position: absolute;
+  z-index: 10;
+  top: 8px;
+`;
--- src/pages/mapPage/MapPage.tsx
+++ src/pages/mapPage/MapPage.tsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 import Maps from "../../components/map/Maps";
+import { Search } from "./MapPage.style";
+import SearchBar from "../../components/map/searchBar/SearchBar";
+import FilterContainer from "../../components/map/filter/FilterContainer";
+import { useEffect, useState } from "react";
 
 export default function MapPage() {
+  const [searchVal, setSearchVal] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (searchVal) {
+      // 지도 좌표 변경 및 api 호출 예정
+      console.log("검색어 : ", searchVal);
+    }
+  }, [searchVal]);
+
   return (
     <Page>
       <Maps />
+      <Search>
+        <SearchBar handleSearchVal={setSearchVal} />
+        <FilterContainer />
+      </Search>
     </Page>
   );
 }
