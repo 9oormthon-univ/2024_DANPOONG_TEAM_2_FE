ad685375b0fed2fd95b3a6ca89f66d222dae5331
Hyun Sang <pycandyq@naver.com>
2024-11-19 13:50:20 +0900

    Init: Login,signup 파일 및 Pages폴더 세팅

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Login: React.FC = () => {
+    return (
+        <div style={{color: 'black'}}>
+            로그인 페이지
+        </div>
+    )
+}
+
+export default Login;
