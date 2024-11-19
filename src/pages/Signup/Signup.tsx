71b81785f1abd0a285ffb48aba73d1f3ddb804a6
Hyun Sang <pycandyq@naver.com>
2024-11-19 19:19:01 +0900

    Feat: 회원가입 페이지 단계 추가

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+// pages/Signup.tsx
+import React, { useState } from "react";
+import StepAgreement from "../../components/Signup/StepAgreement";
+import StepMemberType from "../../components/Signup/StepMemberType";
 import styled from "styled-components";
 
-const Signup : Raeact.FC = () => {
-    
-    return (
-        <div style={{color: 'black'}}>
-            회원가입 페이지
-        </div>
-    )
-}
+export const SignupContainer = styled.div`
+   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  
+  
+`;
+
+const Signup: React.FC = () => {
+  const [currentStep, setCurrentStep] = useState(1); // 현재 단계
+
+  const goToNextStep = () => setCurrentStep((prev) => prev + 1);
+  const goToPreviousStep = () => setCurrentStep((prev) => prev - 1);
+
+  return (
+    <SignupContainer>
+      {currentStep === 1 && <StepAgreement onNext={goToNextStep} />}
+      {currentStep === 2 && (
+        <StepMemberType onNext={goToNextStep} onBack={goToPreviousStep} />
+      )}
+      {/* 이후 단계 추가 */}
+    </SignupContainer>
+  );
+};
 
 export default Signup;
