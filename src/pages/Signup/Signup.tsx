a2021b4510f945950a258206386493756fee05c4
Hyun Sang <pycandyq@naver.com>
2024-11-20 11:56:55 +0900

    Fix: 로그인,회원가입 페이지 수정

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,31 +1,82 @@
-// pages/Signup.tsx
 import React, { useState } from "react";
 import StepAgreement from "../../components/Signup/StepAgreement";
 import StepMemberType from "../../components/Signup/StepMemberType";
 import styled from "styled-components";
+import InvestorForm from "../../components/Signup/StepInvestorForm";
+import StepSocialValue from "../../components/Signup/StepSocialValue";
+import StepWelcomeScreen from "../../components/Signup/StepWelcomeScreen";
 
 export const SignupContainer = styled.div`
-   width: 100%;
+  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
-  
-  
 `;
 
 const Signup: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(1); // 현재 단계
+  const totalSteps = 5; // 총 단계 수
+  const [selectedType, setSelectedType] = useState<string | null>(null); // Investor or Business
 
-  const goToNextStep = () => setCurrentStep((prev) => prev + 1);
-  const goToPreviousStep = () => setCurrentStep((prev) => prev - 1);
+  const goToNextStep = () => {
+    if (currentStep < totalSteps) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  const goToPreviousStep = () => {
+    if (currentStep > 1) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
 
   return (
     <SignupContainer>
-      {currentStep === 1 && <StepAgreement onNext={goToNextStep} />}
-      {currentStep === 2 && (
-        <StepMemberType onNext={goToNextStep} onBack={goToPreviousStep} />
+      {/* Step 1: 동의 단계 */}
+      {currentStep === 1 && (
+        <StepAgreement
+          onNext={goToNextStep}
+          currentStep={currentStep}
+          totalSteps={totalSteps}
+        />
       )}
-      {/* 이후 단계 추가 */}
+
+      {/* Step 2: 회원 유형 선택 */}
+      {currentStep === 2 && (
+        <StepMemberType
+          onNext={goToNextStep}
+          setSelectedType={setSelectedType}
+        />
+      )}
+
+      {/* Step 3: 투자자 폼 선택 */}
+      {currentStep === 3 && selectedType === "investor" && (
+        <InvestorForm
+          onNext={goToNextStep}
+          onBack={goToPreviousStep}
+          currentStep={currentStep}
+          totalSteps={totalSteps}
+        />
+      )}
+    
+      {/* Step 4: 투자자 선택 후 -> 사회적가치 선택 */}
+      {currentStep === 4 && selectedType === "investor" && (
+        <StepSocialValue
+          onNext={goToNextStep}
+          onBack={goToPreviousStep}
+          currentStep={currentStep}
+          totalSteps={totalSteps}
+        />
+      )}
+
+      {/* Step 5: 투자자 선택 후 -> 사회적가치 선택 */}
+      {currentStep === 5 && selectedType === "investor" && (
+        <StepWelcomeScreen
+          onNext={goToNextStep}
+          currentStep={currentStep}
+          totalSteps={totalSteps}
+        />
+      )}
     </SignupContainer>
   );
 };
