ee23e0b56bc7761620d78bf4793b126be0952348
yongmin01 <heart77123@naver.com>
2024-11-22 16:56:25 +0900

    Feat: 마커 여러개 띄우기

--- src/component/map/Maps.tsx
+++ src/component/map/Maps.tsx
@@ -1,18 +1,33 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { projectDetailDummy } from "../../moks/projectDetailDummy";
+import { useNavigate } from "react-router-dom";
 export default function Maps() {
   // 초기 지도 중심 : 카카오 판교 아지트
   const initial_lat = 37.39556485367976;
   const initial_lng = 127.1104141120098;
-
+  const dummy = projectDetailDummy;
+  const navigator = useNavigate();
   return (
     <>
       <Map
         center={{ lat: initial_lat, lng: initial_lng }}
         style={{ width: "100%", height: "100%" }}
       >
-        <MapMarker
-          position={{ lat: initial_lat, lng: initial_lng }}
-        ></MapMarker>
+        {dummy.map((project) => {
+          return (
+            <MapMarker
+              position={{
+                lat: project.address.lat,
+                lng: project.address.lng,
+              }}
+              image={{
+                src: `${project.profileImg}`,
+                size: { width: 39, height: 39 },
+              }}
+              onClick={() => navigator(`/map/${project.id}`)}
+            ></MapMarker>
+          );
+        })}
       </Map>
     </>
   );
