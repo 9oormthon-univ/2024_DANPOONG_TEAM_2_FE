feec6ab413a85056ec056370a5fdf313a96ddab8
yongmin01 <heart77123@naver.com>
2024-11-24 06:42:25 +0900

    Fix: axiosInstance 사용

--- src/apis/deleteScrapStore.ts
+++ src/apis/deleteScrapStore.ts
@@ -1,18 +1,9 @@
-import axios from "axios";
+import axiosInstance from "../component/token";
 
 export const deleteScrapStore = async (storeId: number) => {
-  let config = {
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_TEST_ACCESS_TOKEN}`,
-    },
-  };
-
   try {
     // 스크랩 취소 요청
-    const response = await axios.delete(
-      `${import.meta.env.VITE_API_BASE_URL}/api/store/scrap/${storeId}`,
-      config
-    );
+    const response = await axiosInstance.delete(`/api/store/scrap/${storeId}`);
     return response.status === 200 ? response.data : null;
   } catch (error) {
     console.error("스크랩 취소 실패:", error);
